b5f8f0cdaf54c89187438f5953bd5ccea2428e4f
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-13 10:02:13 +0530

    game played with single player at start position 0

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
 
+    static int position = 0;
+
     public static void main(String[] args) {
+        int start;
+        int position = 0;
+        start = position;
 
         System.out.println("\n...Welcome to the Snake & Ladders Game...\n");
-        
+        System.out.println("Your Start Postion: " + start);
     }
 }
